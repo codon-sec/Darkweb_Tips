a76f9ea36fd559e6a1b1fcf4db50a9de338cdc87
codon-sec <yoshiooyama36@gmail.com>
2020-12-18 22:48:42 +0900

    add intro

--- tor.py
+++ tor.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# 0. brew install tor(Mac)
+# 1. tor (start tor)
+# 2. python3 tor.py
 import requests
 
 proxies = {
-    'http': 'socks5://127.0.0.1:9050',
+    "http": 'socks5://127.0.0.1:9050',
     'https': 'socks5://127.0.0.1:9050'
 }
 
